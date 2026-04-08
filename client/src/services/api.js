dec4030a5a8f86b93ad81ae838d75c1afaeb4f24
usuariopedro1001-bit <usuariopedro1001@gmail.com>
2026-04-08 20:15:58 -0300

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5050/api',
+ baseURL: 'https://crm-1-uyd0.onrender.com/api',
 });
 
 // Add token to requests
